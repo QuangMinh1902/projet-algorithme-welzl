b2484996bb5b925b73088385b4cab512e472d016
Quang Minh PHAM <quangminh1575@gmail.com>
2023-12-22 13:50:45 +0100

    Initial commit

---
+++ src/algorithms/DefaultTeam.java
@@ -0,0 +1,248 @@
+package algorithms;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+
+import supportGUI.Circle;
+import supportGUI.Line;
+
+public class DefaultTeam {
+  public Line calculDiametre(ArrayList<Point> points) {
+    // calculDiametre: ArrayList<Point> --> Line
+    // renvoie une pair de points de la liste qui forme une distance maximum.
+
+    return diameter(points);
+  }
+
+
+  public Circle calculCercleMin(ArrayList<Point> points) {
+    // calculCercleMin: ArrayList<Point> --> Circle
+    //   renvoie un cercle couvrant tout point de la liste, de rayon minimum.
+    //return Ritter(points);
+   // return naif(points);
+    return welzl(points);
+  }
+
+  public ArrayList<Point> enveloppeConvexe(ArrayList<Point> points){
+    if (points.size()<3) {
+      return null;
+    }
+
+    ArrayList<Point> enveloppe = new ArrayList<Point>();
+
+    enveloppe.add(points.get(0));
+    enveloppe.add(points.get(1));
+    enveloppe.add(points.get(2));
+
+
+    /*******************
+     * PARTIE A ECRIRE *
+     *******************/
+    return points;
+  }
+
+  private Line diameter(ArrayList<Point> points) {
+    if (points.size()<2) return null;
+    Point p=points.get(0);
+    Point q=points.get(1);
+    for (Point s: points) for (Point t: points) if (s.distance(t)>p.distance(q)) {p=s;q=t;}
+    return new Line(p,q);
+  }
+
+
+  public Circle naif(ArrayList<Point> inputPoints){
+
+    ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
+
+    if (points.size()<1) return null;
+
+    double cX,cY,cRadius,cRadiusSquared;
+
+    for (Point p: points){
+      for (Point q: points){
+        cX = .5*(p.x+q.x);
+        cY = .5*(p.y+q.y);
+        cRadiusSquared = 0.25*((p.x-q.x)*(p.x-q.x)+(p.y-q.y)*(p.y-q.y));
+        boolean allHit = true;
+        for (Point s: points)
+          if ((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY)>cRadiusSquared){
+            allHit = false;
+            break;
+          }
+        if (allHit) return new Circle(new Point((int)cX,(int)cY),(int)Math.sqrt(cRadiusSquared));
+      }
+    }
+
+    double resX=0;
+    double resY=0;
+    double resRadiusSquared=Double.MAX_VALUE;
+    for (int i=0;i<points.size();i++){
+      for (int j=i+1;j<points.size();j++){
+        for (int k=j+1;k<points.size();k++){
+          Point p=points.get(i);
+          Point q=points.get(j);
+          Point r=points.get(k);
+          //si les trois sont colineaires on passe
+          if ((q.x-p.x)*(r.y-p.y)-(q.y-p.y)*(r.x-p.x)==0) continue;
+          //si p et q sont sur la meme ligne, ou p et r sont sur la meme ligne, on les echange
+          if ((p.y==q.y)||(p.y==r.y)) {
+            if (p.y==q.y){
+              p=points.get(k); //ici on est certain que p n'est sur la meme ligne de ni q ni r
+              r=points.get(i); //parce que les trois points sont non-colineaires
+            } else {
+              p=points.get(j); //ici on est certain que p n'est sur la meme ligne de ni q ni r
+              q=points.get(i); //parce que les trois points sont non-colineaires
+            }
+          }
+          //on cherche les coordonnees du cercle circonscrit du triangle pqr
+          //soit m=(p+q)/2 et n=(p+r)/2
+          double mX=.5*(p.x+q.x);
+          double mY=.5*(p.y+q.y);
+          double nX=.5*(p.x+r.x);
+          double nY=.5*(p.y+r.y);
+          //soit y=alpha1*x+beta1 l'equation de la droite passant par m et perpendiculaire a la droite (pq)
+          //soit y=alpha2*x+beta2 l'equation de la droite passant par n et perpendiculaire a la droite (pr)
+          double alpha1=(q.x-p.x)/(double)(p.y-q.y);
+          double beta1=mY-alpha1*mX;
+          double alpha2=(r.x-p.x)/(double)(p.y-r.y);
+          double beta2=nY-alpha2*nX;
+          //le centre c du cercle est alors le point d'intersection des deux droites ci-dessus
+          cX=(beta2-beta1)/(double)(alpha1-alpha2);
+          cY=alpha1*cX+beta1;
+          cRadiusSquared=(p.x-cX)*(p.x-cX)+(p.y-cY)*(p.y-cY);
+          if (cRadiusSquared>=resRadiusSquared) continue;
+          boolean allHit = true;
+          for (Point s: points)
+            if ((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY)>cRadiusSquared){
+              allHit = false;
+              break;
+            }
+          if (allHit) {System.out.println("Found r="+Math.sqrt(cRadiusSquared));resX=cX;resY=cY;resRadiusSquared=cRadiusSquared;}
+        }
+      }
+    }
+
+    return new Circle(new Point((int)resX,(int)resY),(int)Math.sqrt(resRadiusSquared));
+  }
+
+
+  private Circle Ritter(ArrayList<Point> points){
+    if (points.size()<1) return null;
+    ArrayList<Point> rest = (ArrayList<Point>)points.clone();
+    Point dummy=rest.get(0);
+    Point p=dummy;
+    for (Point s: rest) if (dummy.distance(s)>dummy.distance(p)) p=s;
+    Point q=p;
+    for (Point s: rest) if (p.distance(s)>p.distance(q)) q=s;
+    double cX=.5*(p.x+q.x);
+    double cY=.5*(p.y+q.y);
+    double cRadius=.5*p.distance(q);
+    rest.remove(p);
+    rest.remove(q);
+    while (!rest.isEmpty()){
+      Point s=rest.remove(0);
+      double distanceFromCToS=Math.sqrt((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY));
+      if (distanceFromCToS<=cRadius) continue;
+      double cPrimeRadius=.5*(cRadius+distanceFromCToS);
+      double alpha=cPrimeRadius/(double)(distanceFromCToS);
+      double beta=(distanceFromCToS-cPrimeRadius)/(double)(distanceFromCToS);
+      double cPrimeX=alpha*cX+beta*s.x;
+      double cPrimeY=alpha*cY+beta*s.y;
+      cRadius=cPrimeRadius;
+      cX=cPrimeX;
+      cY=cPrimeY;
+    }
+    return new Circle(new Point((int)cX,(int)cY),(int)cRadius);
+  }
+
+
+
+/** circle de l'algorithme de  Welzl */
+
+  public Circle welzl(ArrayList<Point> points) {
+    return bMinDisk(points, new ArrayList<Point>());
+  }
+
+  private Circle bMinDisk(ArrayList<Point> Ps, ArrayList<Point> R) {
+    ArrayList<Point> P = new ArrayList<Point>(Ps);
+    Random random = new Random();
+    Circle cercle = null;
+
+    if (P.isEmpty() || R.size() == 3) {
+      cercle = bmd(new ArrayList<Point>(), R);
+
+    } else {
+      Point p = P.get((random.nextInt(P.size())));
+      P.remove(p);
+
+      cercle = bMinDisk(P, R);
+
+      if (cercle != null && !contains(cercle, p)) {
+        R.add(p);
+        cercle = bMinDisk(P, R);
+        R.remove(p);
+      }
+    }
+
+    return cercle;
+  }
+
+
+  private boolean contains(Circle c, Point p) {
+    if (p.distance(c.getCenter()) - c.getRadius() < 0.00001) {
+      return true;
+    }
+    return false;
+  }
+
+
+  private Circle bmd(ArrayList<Point> P, ArrayList<Point> R) {
+
+    if (P.isEmpty() && R.size() == 0)
+      return new Circle(new Point(0, 0), 10);
+
+    Random r = new Random();
+    Circle cercle = null;
+
+    if (R.size() == 1) {
+      cercle = new Circle(R.get(0), 0);
+    }
+
+    if (R.size() == 2) {
+      double cx = (R.get(0).x + R.get(1).x) / 2;
+      double cy = (R.get(0).y + R.get(1).y) / 2;
+      double d = R.get(0).distance(R.get(1)) / 2;
+      Point p = new Point((int) cx, (int) cy);
+      cercle = new Circle(p, (int) Math.ceil(d));
+    } else {
+      if (R.size() == 3)
+        cercle = circle3point(R.get(0), R.get(1), R.get(2));
+    }
+
+    return cercle;
+  }
+
+
+  private int norm(Point a) {
+    return (a.x * a.x) + (a.y * a.y);
+  }
+
+  private Circle circle3point(Point a, Point b, Point c) {
+    double d = (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) * 2;
+
+    if (d == 0) return null;
+
+    double x = ((norm(a) * (b.y - c.y)) + (norm(b) * (c.y - a.y)) + (norm(c) * (a.y - b.y))) / d;
+
+    double y = ((norm(a) * (c.x - b.x)) + (norm(b) * (a.x - c.x)) + (norm(c) * (b.x - a.x))) / d;
+
+    Point p = new Point((int) x, (int) y);
+
+    return new Circle(p, (int) Math.ceil(p.distance(a)));
+
+  }
+
+}
---
+++ src/algorithms/main.java
@@ -0,0 +1,43 @@
+package algorithms;
+
+import java.awt.Point;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import supportGUI.Circle;
+
+public class main {
+
+	public static void main(String[] args) {
+		long debut = System.currentTimeMillis();
+		DefaultTeam dt = new DefaultTeam();
+		String path = "samples/";
+		File[] listFic = Utils.listeRepertoire(path);
+
+		try (PrintWriter writer = new PrintWriter(new FileWriter("output.csv"))) {
+			// Write header to CSV file
+			writer.println("Fichier - WelzlTime - NaifTime");
+
+			for (File x : listFic) {
+				ArrayList<Point> points = Utils.readFile(x.getPath());
+
+				long debutW = System.nanoTime();
+				Circle cw = dt.welzl(points);
+				long tempsW = System.nanoTime() - debutW;
+
+				long debutN = System.nanoTime();
+				Circle cn = dt.naif(points);
+				long tempsN = System.nanoTime() - debutN;
+
+				writer.println(x.getPath() + " - " + tempsW + " - " + tempsN);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(System.currentTimeMillis() - debut);
+	}
+}
