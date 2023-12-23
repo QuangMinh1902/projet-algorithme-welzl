bd85fc72ccc608e44c4b367ff2cf01de3944da1f
Quang Minh PHAM <quangminh1575@gmail.com>
2023-12-23 15:51:29 +0100

    update

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -19,7 +19,7 @@ public class DefaultTeam {
   public Circle calculCercleMin(ArrayList<Point> points) {
     // calculCercleMin: ArrayList<Point> --> Circle
     //   renvoie un cercle couvrant tout point de la liste, de rayon minimum.
-    //return Ritter(points);
+
    // return naif(points);
     return welzl(points);
   }
@@ -111,34 +111,6 @@ public class DefaultTeam {
     return new Circle(new Point((int)resX,(int)resY),(int)Math.sqrt(resRadiusSquared));
   }
 
- // private Circle Ritter(ArrayList<Point> points){
-    if (points.size()<1) return null;
-    ArrayList<Point> rest = (ArrayList<Point>)points.clone();
-    Point dummy=rest.get(0);
-    Point p=dummy;
-    for (Point s: rest) if (dummy.distance(s)>dummy.distance(p)) p=s;
-    Point q=p;
-    for (Point s: rest) if (p.distance(s)>p.distance(q)) q=s;
-    double cX=.5*(p.x+q.x);
-    double cY=.5*(p.y+q.y);
-    double cRadius=.5*p.distance(q);
-    rest.remove(p);
-    rest.remove(q);
-    while (!rest.isEmpty()){
-      Point s=rest.remove(0);
-      double distanceFromCToS=Math.sqrt((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY));
-      if (distanceFromCToS<=cRadius) continue;
-      double cPrimeRadius=.5*(cRadius+distanceFromCToS);
-      double alpha=cPrimeRadius/(double)(distanceFromCToS);
-      double beta=(distanceFromCToS-cPrimeRadius)/(double)(distanceFromCToS);
-      double cPrimeX=alpha*cX+beta*s.x;
-      double cPrimeY=alpha*cY+beta*s.y;
-      cRadius=cPrimeRadius;
-      cX=cPrimeX;
-      cY=cPrimeY;
-    }
-    return new Circle(new Point((int)cX,(int)cY),(int)cRadius);
-  }
 
 /** circle de l'algorithme de  Welzl */
   public Circle welzl(ArrayList<Point> points) {
